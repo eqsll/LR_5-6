dd455be0ada999e59a1eb2030c298a901446ade2
eqsll <144461926+eqsll@users.noreply.github.com>
2023-10-16 16:40:43 +0300

    Add files via upload

---
+++ блок 1 задача 1 Лысков.pas
@@ -0,0 +1,13 @@
+﻿const n=20;
+var i: integer;
+a: array [1..n] of real;
+begin
+  writeln ('Введите 20 чисел');
+  for i:= 1 to n do
+  read (a[i]);
+  for i:= 1 to n do if a[i] > 0 then
+    a[i]:=0;
+  for i:= 1 to n do if a[i] < 0 then
+    a[i]:= -a[i]**2;
+      write (a);
+end.
